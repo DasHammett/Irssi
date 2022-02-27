8830fc7ae5edf7172d5404793dca399d2e572484
DasHammett <clockwork80@gmail.com>
2022-02-27 13:52:15 +0100

    Updated track_join

--- previously_known.pl
+++ previously_known.pl
@@ -50,15 +50,24 @@ sub track_join {
     my $joined_nick= conv($joined_nick);
     my @spl   = split(/@/, $address);
     my $mask  = $spl[1];
-  
+    $previous = "";
+    
     if (! (exists $hash{$mask} )) {
         $hash{$mask} = $joined_nick;
-        $previous = ""
     } else {
-       $previous = "Previously known as: " . $hash{$mask};
-       $hash{$mask} = $hash{$mask} . ", $joined_nick"
-     }
-}
+       my @nicks_list = split(/, /,$hash{$mask});
+       my @old_nicks = grep(!/$joined_nick/,@nicks_list);
+       my $nick_in_list = grep(/$joined_nick/,@nicks_list);
+       if (!$nick_in_list) {
+          $hash{$mask} = $hash{$mask} . ", $joined_nick";
+          $previous = "Previously known as: " . join(", ", @nicks_list);
+       } else {
+          if (@old_nicks) {
+             $previous = "Previously known as: " . join(", ", @old_nicks);
+          }
+       }
+    }
+ }
 
 sub save_to_file {
    open(FH , ">", $nickdb) or die $!;
@@ -112,4 +121,5 @@ Irssi::signal_add("message join", \&track_join);
 Irssi::signal_add_first("server quit", \&save_to_file);
 Irssi::signal_add("message nick", \&nick_changed);
 Irssi::command_bind("previous_nick_save", \&save_to_file);
+Irssi::command_bind("previous_nick_count", \&hash_count);
 Irssi::command_bind("previous_nick", \&search_previous)
